1a1aad553a5e9d2259a4c229f024dbfc68314499
admin <saidsadyk2907@gmail.com>
2024-10-22 13:17:32 +0600

    commit

--- src/Account.java
+++ src/Account.java
@@ -56,10 +56,10 @@ public class Account {
             if(id==account.id){
                 double deposit = scanner.nextDouble();
                 if (account.balance-deposit>=0){
-                account.balance = account.balance - deposit;}}
+                account.balance = account.balance - deposit;}
             else {
                 System.out.println("акча жетишсиз");
-            }
+            }}
     }}
 
     public void transaction(int id , Account[]accounts) {
@@ -72,10 +72,14 @@ public class Account {
             double withdrawal1 = scanner1.nextDouble();
             if(account.balance-withdrawal1>=0){
             for (Account ids : accounts) {
+                if(idUser==id){
+                    System.out.println("Error");
+                }
+                else {
                 if (idUser == ids.id) {
                     ids.balance = ids.balance + withdrawal1;
                     account.balance=account.balance-withdrawal1;
-                }
+                }}
             }}else {
                 System.out.println("акча жетишсиз");
             }
@@ -91,6 +95,48 @@ public class Account {
             }
         }
         System.out.println("андай ID жок");
+    }
+    public static void l(Account[]accounts){
+        for (int j = 0; j < 1; ) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("напишите ваш ID");
+            int id = scanner.nextInt();
+
+            boolean check = false;
+
+            for (Account account : accounts) {
+                if (id == account.getId()) {
+                    check = true;
+
+                    while (true) {
+                        System.out.println("Menu \n1;balance\n2;deposit\n3;withdrawal\n4;transaction\n5;Status account\n0;Exit");
+                        int menu = scanner.nextInt();
+
+                        if (menu == 0) {
+                            break;
+                        } else {
+                            if (menu == 1) {
+                                account.checkBalance(id, accounts);
+                            }
+                            if (menu == 2) {
+                                account.deposit(id, accounts);
+                            }
+                            if (menu == 3) {
+                                account.withdrawal(id, accounts);
+                            }
+                            if (menu == 4) {
+                                account.transaction(id, accounts);
+                            }
+                            if (menu == 5) {
+                                account.tet(id, accounts);
+                            }
+
+                        }
+                    }
+                    break;
+                }
+            }
+        }
     }
     @Override
     public String toString() {
